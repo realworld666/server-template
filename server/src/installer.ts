ee94b7faa609f0d7e23d92c7a05f3044fc6fa9f3
Dave Conley <david.conley@telefonica.com>
2022-04-25 09:55:17 +0100

    Register installers as singletons so that we dont end up with multiple instances of the config object

--- server/src/installer.ts
+++ server/src/installer.ts
@@ -1,4 +1,4 @@
-import { container } from 'tsyringe';
+import { container, Lifecycle } from 'tsyringe';
 import IamService from './services/iam-service';
 import AwsIamService from './services/aws/auth/aws-iam-service';
 import MockIamService from './services/mock/auth/mock-iam-service';
@@ -11,16 +11,16 @@ import { MockAppConfigService } from './services/mock/config/mock-app-config-ser
  * Install the current services for the app
  */
 export function installServices(type: string) {
-  container.register<AppInterface>('AppInterface', { useClass: App });
+  container.register<AppInterface>('AppInterface', { useClass: App }, { lifecycle: Lifecycle.Singleton });
   switch (type) {
     case 'test': {
       // Configure the IAM service to use local mock
-      container.register<IamService>('IamService', { useClass: MockIamService });
+      container.register<IamService>('IamService', { useClass: MockIamService }, { lifecycle: Lifecycle.Singleton });
       break;
     }
     case 'cognito': {
       // Configure the IAM service to use AWS
-      container.register<IamService>('IamService', { useClass: AwsIamService });
+      container.register<IamService>('IamService', { useClass: AwsIamService }, { lifecycle: Lifecycle.Singleton });
       break;
     }
     default: {
@@ -33,7 +33,11 @@ export function installServices(type: string) {
  * As above but used by the unit tests so we should set up and global mock services here
  */
 export function installMockServices() {
-  container.register<AppInterface>('AppInterface', { useClass: App });
-  container.register<AppConfigService>(AppConfigService, { useClass: MockAppConfigService });
-  container.register<IamService>('IamService', { useClass: MockIamService });
+  container.register<AppInterface>('AppInterface', { useClass: App }, { lifecycle: Lifecycle.Singleton });
+  container.register<AppConfigService>(
+    AppConfigService,
+    { useClass: MockAppConfigService },
+    { lifecycle: Lifecycle.Singleton }
+  );
+  container.register<IamService>('IamService', { useClass: MockIamService }, { lifecycle: Lifecycle.Singleton });
 }
