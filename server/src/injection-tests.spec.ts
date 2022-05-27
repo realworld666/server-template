0898c67268e561621e31e9fc9618c6500a2c176b
Dave Conley <david.conley@telefonica.com>
2022-05-27 11:03:12 +0100

    Fix for configurables being instantiated twice and added unit test to ensure that the same object is returned regardless of whether its injected by its core type or the configurable type

---
+++ server/src/injection-tests.spec.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { container } from 'tsyringe';
+import { expect } from 'chai';
+import MockIamService from './services/mock/auth/mock-iam-service';
+import IamService from './services/common/iam/iam-service';
+import { Configurable } from './services/common/config/configurable';
+
+describe('Injection Tests', () => {
+  it('should return the same object', async () => {
+    const iamService = container.resolve<IamService>('IamService');
+    const configurables = container.resolveAll<Configurable>('Configurable');
+
+    let foundConfigurable = false;
+    expect(iamService).to.be.instanceof(MockIamService);
+    // @ts-ignore
+    iamService.test = 'test';
+    for (let i = 0; i < configurables.length; i++) {
+      if (configurables[i] instanceof MockIamService) {
+        foundConfigurable = true;
+        // @ts-ignore
+        expect(configurables[i].test).to.eq(
+          'test',
+          'Configurable and IamService are different instances of the same object'
+        );
+      }
+    }
+    expect(foundConfigurable).to.be.true;
+  });
+});
--- server/src/installer.ts
+++ server/src/installer.ts
@@ -1,19 +1,18 @@
 import { container, Lifecycle } from 'tsyringe';
-import IamService from './services/common/iam/iam-service';
 import AwsIamService from './services/aws/auth/aws-iam-service';
 import MockIamService from './services/mock/auth/mock-iam-service';
 import AppInterface from './app-interface';
 import App from './app';
 import AppConfigService from './services/common/config/app-config-service';
 import { MockAppConfigService } from './services/mock/config/mock-app-config-service';
-import { DatabaseService } from './services/common/database/database-service';
 import DynamoDbService from './services/aws/database/dynamo-db-service';
 import MockDatabaseService from './services/mock/database/mock-database-service';
+import { Configurable } from './services/common/config/configurable';
 
-function registerConfigurables(param: any[]) {
-  param.forEach((service) => {
-    container.register('Configurable', { useClass: service }, { lifecycle: Lifecycle.Singleton });
-  });
+function registerConfigurable<T extends Configurable>(Service: { new (): T }, token: string) {
+  const value = new Service();
+  container.register<Configurable>('Configurable', { useValue: value });
+  container.register<T>(token, { useValue: value });
 }
 
 /**
@@ -24,24 +23,14 @@ export function installServices(type: string) {
   switch (type) {
     case 'test': {
       // Configure the IAM service to use local mock
-      container.register<IamService>('IamService', { useClass: MockIamService }, { lifecycle: Lifecycle.Singleton });
-      container.register<DatabaseService>(
-        'DatabaseService',
-        { useClass: MockDatabaseService },
-        { lifecycle: Lifecycle.Singleton }
-      );
-      registerConfigurables([MockIamService, MockDatabaseService]);
+      registerConfigurable(MockIamService, 'IamService');
+      registerConfigurable(MockDatabaseService, 'DatabaseService');
       break;
     }
     case 'aws': {
       // Install the aws services
-      container.register<IamService>('IamService', { useClass: AwsIamService }, { lifecycle: Lifecycle.Singleton });
-      container.register<DatabaseService>(
-        'DatabaseService',
-        { useClass: DynamoDbService },
-        { lifecycle: Lifecycle.Singleton }
-      );
-      registerConfigurables([AwsIamService, DynamoDbService]);
+      registerConfigurable(AwsIamService, 'IamService');
+      registerConfigurable(DynamoDbService, 'DatabaseService');
       break;
     }
     default: {
@@ -55,16 +44,11 @@ export function installServices(type: string) {
  */
 export function installMockServices() {
   container.register<AppInterface>('AppInterface', { useClass: App }, { lifecycle: Lifecycle.Singleton });
-  container.register<IamService>('IamService', { useClass: MockIamService }, { lifecycle: Lifecycle.Singleton });
   container.register<AppConfigService>(
     AppConfigService,
     { useClass: MockAppConfigService },
     { lifecycle: Lifecycle.Singleton }
   );
-  container.register<DatabaseService>(
-    'DatabaseService',
-    { useClass: MockDatabaseService },
-    { lifecycle: Lifecycle.Singleton }
-  );
-  registerConfigurables([MockIamService, MockDatabaseService]);
+  registerConfigurable(MockIamService, 'IamService');
+  registerConfigurable(MockDatabaseService, 'DatabaseService');
 }
