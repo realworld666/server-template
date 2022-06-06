4d41b90663f55857e304f7db6b2a6d9225278d9f
Dave Conley <david.conley@telefonica.com>
2022-06-06 11:22:59 +0100

    Change to credential parsing

--- server/src/services/google/bootstrap/firebase-service.ts
+++ server/src/services/google/bootstrap/firebase-service.ts
@@ -11,9 +11,17 @@ export default class FirebaseService implements CloudServiceBootstrap {
 
   async init(): Promise<void> {
     if (!FirebaseService.initialized) {
-      this.app = admin.initializeApp({
-        credential: applicationDefault(),
-      });
+      // if the environment variable contains a JSON object, parse it
+      if (process.env.GOOGLE_APPLICATION_CREDENTIALS!.charAt(0) === '{') {
+        this.app = admin.initializeApp({
+          credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS!)),
+        });
+        // else assume this contains a path
+      } else {
+        this.app = admin.initializeApp({
+          credential: applicationDefault(),
+        });
+      }
       FirebaseService.initialized = true;
     } else {
       this.app = admin.app();
